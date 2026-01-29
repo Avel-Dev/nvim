d184f8db20be6b9e01ed7590b5455cd6c74b37fb
Avel-Dev <arumugaveldev@gmail.com>
2026-01-30 00:26:18 +0530

    gas syntax for nvim

---
+++ ftdetect/asm.vim
@@ -0,0 +1,5 @@
+
+augroup gas_ft
+  autocmd!
+  autocmd BufRead,BufNewFile *.s setfiletype asm
+augroup END
--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,3 +1,6 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.cmd("syntax on")
+vim.cmd("filetype plugin on")
+
 require("core.keymaps")
