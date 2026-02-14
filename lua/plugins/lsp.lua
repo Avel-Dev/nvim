ce61514ac4869c5292edc8bdc2028ee65cd0a2a9
Avel-Dev <arumugaveldev@gmail.com>
2026-02-15 00:45:40 +0530

    cmake

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			vim.lsp.config("clangd", {
+				capabilities = capabilities,
+			})
+
+			-- LSP keymaps
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover)
+			vim.keymap.set("n", "gr", vim.lsp.buf.references)
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+		end,
+	},
+}
